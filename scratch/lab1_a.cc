289e2781a3d8ceb02876d8b158cdf8e2db382503
Shubham Maurya <code.shaurya@gmail.com>
2020-01-28 14:59:48 -0500

    tutorials were added

---
+++ scratch/lab1_a.cc
@@ -0,0 +1,177 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2006,2007 INRIA
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */ 
+
+#include "ns3/core-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/network-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/yans-wifi-helper.h"
+#include "ns3/ssid.h"
+#include "ns3/netanim-module.h"
+#include "ns3/flow-monitor.h"
+#include "ns3/flow-monitor-helper.h"
+#include<stdlib.h>
+#include<time.h>
+
+
+using namespace ns3;
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+Config::SetDefault ("ns3::RandomWalk2dMobilityModel::Mode", StringValue ("Time"));
+  Config::SetDefault ("ns3::RandomWalk2dMobilityModel::Time", StringValue ("2s"));
+  Config::SetDefault ("ns3::RandomWalk2dMobilityModel::Speed", StringValue ("ns3::ConstantRandomVariable[Constant=1.0]"));
+  Config::SetDefault ("ns3::RandomWalk2dMobilityModel::Bounds", StringValue ("0|200|0|200"));  
+
+bool verbose = true;
+  uint32_t nWifi = 4;
+ 
+  CommandLine cmd;
+  cmd.AddValue ("nWifi", "Number of wifi STA devices", nWifi);
+  cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
+  cmd.Parse (argc, argv);
+
+if (nWifi > 18)
+    {
+      std::cout << "nWifi should be 18 or less; otherwise grid layout exceeds the bounding box" << std::endl;
+      return 1;
+    }
+
+  if (verbose)
+    {
+      LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+      LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    }
+// create an array of empty nodes for testing purposes 
+  
+  NodeContainer nodes;
+  nodes.Create (nWifi);
+  
+  YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();
+  YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();
+  phy.SetChannel (channel.Create ());
+
+  WifiHelper wifi;
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager");
+
+  WifiMacHelper mac;
+  mac.SetType ("ns3::AdhocWifiMac");
+
+  NetDeviceContainer devices;
+  devices = wifi.Install (phy, mac, nodes);
+
+
+  MobilityHelper mobility;
+  
+  // setup the grid itself: objects are laid out
+  // started from (0,0) with 4 objects per row, 
+  // the x interval between each object is 5 meters
+  // and the y interval between each object is 5 meters
+  
+     /*mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
+                                 "MinX", DoubleValue (0.0),
+                                 "MinY", DoubleValue (0.0),
+                                 "DeltaX", DoubleValue (100.0),
+                                 "DeltaY", DoubleValue (100.0),
+                                 "GridWidth", UintegerValue (4),
+                                 "LayoutType", StringValue ("RowFirst"));*/
+  // each object will be attached a static position.
+  // i.e., once set by the "position allocator", the
+  // position will never change.
+
+  /*Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
+  positionAlloc->Add (Vector (0.0, 0.0, 0.0));
+  positionAlloc->Add (Vector (20.0, 0.0, 0.0));
+  positionAlloc->Add (Vector (40.0, 0.0, 0.0));
+  positionAlloc->Add (Vector (60.0, 0.0, 0.0));
+  mobility.SetPositionAllocator (positionAlloc);
+     
+  mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
+                             "Bounds", RectangleValue (Rectangle (-200, 200, -200, 200)));
+  
+   //mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+
+  // finalize the setup by attaching to each object
+  // in the input array a position and initializing
+  // this position with the calculated coordinates.
+  
+  mobility.Install (nodes);*/
+
+  mobility.SetPositionAllocator ("ns3::RandomDiscPositionAllocator",
+                                 "X", StringValue ("100.0"),
+                                 "Y", StringValue ("100.0"),
+                                 "Rho", StringValue ("ns3::UniformRandomVariable[Min=0|Max=30]"));
+  mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
+                             "Mode", StringValue ("Time"),
+                             "Time", StringValue ("2s"),
+                             "Speed", StringValue ("ns3::ConstantRandomVariable[Constant=1.0]"),
+                             "Bounds", StringValue ("0|200|0|200"));
+  mobility.InstallAll ();
+  
+  InternetStackHelper stack;
+  stack.Install (nodes);
+  
+  Ipv4AddressHelper address;
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+  
+  Ipv4InterfaceContainer wifiInterfaces;
+  wifiInterfaces = address.Assign (devices);
+
+  UdpEchoServerHelper echoServer (9);
+
+ srand(1);  
+ int s=rand()%nWifi;
+ int d=rand()%nWifi;
+
+if(s!=d)
+{
+  ApplicationContainer serverApps = echoServer.Install (nodes.Get (d));
+  serverApps.Start (Seconds (0.0));
+  serverApps.Stop (Seconds (10.0));
+
+  UdpEchoClientHelper echoClient (wifiInterfaces.GetAddress (d), 9);
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (2));
+  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1)));
+  echoClient.SetAttribute ("PacketSize", UintegerValue (2048));
+
+  ApplicationContainer clientApps = echoClient.Install (nodes.Get (s));
+  clientApps.Start (Seconds (0.5));
+  clientApps.Stop (Seconds (10.0));
+
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+}
+  //flowmonitor
+
+  Ptr<FlowMonitor> flowmonitor;
+  FlowMonitorHelper flowhelper;
+  flowmonitor=flowhelper.InstallAll();
+  
+  Simulator::Stop (Seconds (10.0));
+
+   
+  AnimationInterface anim("mycode.xml");
+  
+  Simulator::Run ();
+  flowmonitor->SerializeToXmlFile("flowmon.xml",true,true);
+  Simulator::Destroy ();
+  return 0;
+}
